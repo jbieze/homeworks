e1d091c9e7119f7d1d379643f0509f10ca4c89b7
jbieze <joseph.bieze@gmail.com>
2017-08-07 23:48:31 -0700

    Add files via upload

---
+++ W2D2/skeleton/lib/board.rb
@@ -0,0 +1,47 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @cups = Array.new(14) { Array.new }
+    @name1 = name1
+    @name2 = name2
+    place_stones
+  end
+
+  def place_stones
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos >= 13 || start_pos <= 6
+  end
+
+  def make_move(start_pos, current_player_name)
+
+    @cups[start_pos] = []
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine what #make_move returns
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[1..6].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
+  end
+
+  def winner
+  end
+end
