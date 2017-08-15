2b5c63fef7c26d545c1384ad5daa90e2a0747a83
Joey Bieze <joseph.bieze@gmail.com>
2017-08-14 23:55:41 -0700

    Playwrights class methods and instance methods

--- W3D2/plays_playwrights/plays.rb
+++ W3D2/plays_playwrights/plays.rb
@@ -70,5 +70,65 @@ class Play
         playwright = ?
     SQL
   end
+end
 
+class playwright
+  attr_accessor :name, :birth_year, :id
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELCT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    scribe = PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+      SQL
+      @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    plays = PlayDBConnection.instance.execute(<<-SQL, @id)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    plays.map { |play| Play.new(play) }
+  end
 end
