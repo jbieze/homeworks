45e682006eefee4c7b6b2a00cd27e05bb4e52bc0
Joey Bieze <joseph.bieze@gmail.com>
2017-08-17 22:47:51 -0700

    #method_missing

--- W3D5/Metacorgis/meta_corgis.rb
+++ W3D5/Metacorgis/meta_corgis.rb
@@ -72,35 +72,35 @@ class SnackBox
   end
 end
 
-class CorgiSnacks
-
-  def initialize(snack_box, box_id)
-    @snack_box = snack_box
-    @box_id = box_id
-  end
-
-  def bone
-    info = @snack_box.get_bone_info(@box_id)
-    tastiness = @snack_box.get_bone_tastiness(@box_id)
-    result = "Bone: #{info}: #{tastiness} "
-    tastiness > 30 ? "* #{result}" : result
-  end
-
-  def kibble
-    info = @snack_box.get_kibble_info(@box_id)
-    tastiness = @snack_box.get_kibble_tastiness(@box_id)
-    result = "Kibble: #{info}: #{tastiness} "
-    tastiness > 30 ? "* #{result}" : result
-  end
-
-  def treat
-    info = @snack_box.get_treat_info(@box_id)
-    tastiness = @snack_box.get_treat_tastiness(@box_id)
-    result = "Treat: #{info}: #{tastiness} "
-    tastiness > 30 ? "* #{result}" : result
-  end
-
-end
+# class CorgiSnacks
+# 
+#   def initialize(snack_box, box_id)
+#     @snack_box = snack_box
+#     @box_id = box_id
+#   end
+#
+#   def bone
+#     info = @snack_box.get_bone_info(@box_id)
+#     tastiness = @snack_box.get_bone_tastiness(@box_id)
+#     result = "Bone: #{info}: #{tastiness} "
+#     tastiness > 30 ? "* #{result}" : result
+#   end
+#
+#   def kibble
+#     info = @snack_box.get_kibble_info(@box_id)
+#     tastiness = @snack_box.get_kibble_tastiness(@box_id)
+#     result = "Kibble: #{info}: #{tastiness} "
+#     tastiness > 30 ? "* #{result}" : result
+#   end
+#
+#   def treat
+#     info = @snack_box.get_treat_info(@box_id)
+#     tastiness = @snack_box.get_treat_tastiness(@box_id)
+#     result = "Treat: #{info}: #{tastiness} "
+#     tastiness > 30 ? "* #{result}" : result
+#   end
+#
+# end
 
 
 class MetaCorgiSnacks
