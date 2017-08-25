1187cf5e9f4d061e0af61d2ff3a7f3f15a6ef87e
Joey Bieze <joseph.bieze@gmail.com>
2017-08-24 23:29:19 -0700

    Topic 2: Layouts

--- W4D5/solution/app/helpers/application_helper.rb
+++ W4D5/solution/app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
-  "<input
-    type="hidden"
-    name="authenticity_token"
-    value='#{form_authenticity_token}'
-  />".html_safe
+  def auth_token
+    "<input
+      type='hidden'
+      name='authenticity_token'
+      value='#{form_authenticity_token}'>".html_safe
+  end
 end
