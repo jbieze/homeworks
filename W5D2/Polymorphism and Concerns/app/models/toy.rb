b9077e502a836b5fcf7e2e22aeb32d97c2082600
Joey Bieze <joseph.bieze@gmail.com>
2017-08-28 23:40:38 -0700

    Phase 1: Create Polymophic Associations

--- W5D2/Polymorphism and Concerns/app/models/toy.rb
+++ W5D2/Polymorphism and Concerns/app/models/toy.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  toyable_type :string
+#  toyable_id   :integer
+#
+
 class Toy < ApplicationRecord
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
--- W5D2/Polymorphism and Concerns/test/models/toy_test.rb
+++ W5D2/Polymorphism and Concerns/test/models/toy_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  toyable_type :string
+#  toyable_id   :integer
+#
+
 require 'test_helper'
 
 class ToyTest < ActiveSupport::TestCase
