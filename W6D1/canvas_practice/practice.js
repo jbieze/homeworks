8ffa0ff6570d52ab058eb375bde6f984fd9a1e1a
Joey Bieze <joseph.bieze@gmail.com>
2017-09-04 00:15:25 -0700

    Finish Phase IV: Hexagon

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -6,11 +6,24 @@ document.addEventListener("DOMContentLoaded", function(){
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, 500, 500);
 
+  // ctx.beginPath();
+  // ctx.arc(250, 250, 125, 0, 2*Math.PI);
+  // ctx.strokeStyle = "blue";
+  // ctx.lineWidth = 2;
+  // ctx.stroke();
+  // ctx.fillStyle = "skyblue";
+  // ctx.fill();
+
   ctx.beginPath();
-  ctx.arc(250, 250, 125, 0, 2*Math.PI);
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 2;
-  ctx.stroke();
-  ctx.fillStyle = "skyblue";
+  ctx.moveTo(250, 250);
+  ctx.lineTo(400, 250);
+  ctx.lineTo(325, 375);
+  ctx.lineTo(175, 375);
+  ctx.lineTo(100, 250);
+  ctx.lineTo(175, 125);
+  ctx.lineTo(325, 125);
+  ctx.lineTo(400, 250);
+  ctx.fillStyle = "blue";
   ctx.fill();
+
 });
