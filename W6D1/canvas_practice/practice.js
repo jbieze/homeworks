1e9f9be7e3e21aaf0bb2765ead4426f91b72f451
Joey Bieze <joseph.bieze@gmail.com>
2017-09-04 00:01:57 -0700

    Draw cicle

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -5,4 +5,10 @@ document.addEventListener("DOMContentLoaded", function(){
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(250, 250, 125, 0, 2*Math.PI);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 2;
+  ctx.stroke();
 });
